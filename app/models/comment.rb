1c37ef5ac454c5c7eb02b09785ef30fc9b3a265b
Rocky Chiang <rocky@chem.ucla.edu>
2018-04-18 00:55:33 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: :reject_comment
+
+  def reject_comment(attributes)
+    attributes['username'].blank?
+  end
 
 end
